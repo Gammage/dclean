4e2e46f32f3c9a9a6491e597cf97807f497a96ec
ben <30186471+Gammage@users.noreply.github.com>
2026-07-03 21:12:43 +0100

    added argparse

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from dclean!")
+import pandas as pd
+import argparse
+
+def main(file_path):
+    
+    df = pd.read_csv(file_path)
+
+    df = df.drop_duplicates()
+
+    output_file = "cleaned_" + file_path.split("/")[-1]
+    df.to_csv(output_file, index=False)
+    print(f"dropped duplicates, output {output_file}")
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help="Path to the CSV file")
+
+    args = parser.parse_args()
+    main(args.file)
