fa0435879915da3053e70216a34c5e44f2a1b06e
ben <30186471+Gammage@users.noreply.github.com>
2026-07-06 01:51:00 +0100

    added commands via argparse

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import pandas as pd
 import argparse
 
-def main(file_path):
+def clean_data(df):
+    return df.drop_duplicates()
+
+def summarise_data(df):
+    print(df.shape)
+    print(df.columns)
+    print(df.isnull().sum())
+    print(df.describe())   
+
+def main(file_path, info, clean):
     
     df = pd.read_csv(file_path)
 
-    df = df.drop_duplicates()
+    if clean:
+        df = clean_data(df)
+
+    if info:
+        summarise_data(df)
 
     output_file = "cleaned_" + file_path.split("/")[-1]
     df.to_csv(output_file, index=False)
-    print(f"dropped duplicates, output {output_file}")
+    print(f"saved{output_file}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("file", help="Path to the CSV file")
+    parser.add_argument("--info", action="store_true", help="summarises the file")
+    parser.add_argument("--clean", action="store_true", help="removes duplicates and other dirty data")
 
     args = parser.parse_args()
-    main(args.file)
+    main(args.file, args.info, args.clean)
+
+
